32ebed2eb53d66d205ec31c5f59fe5533eb602ee
Diaa Hassan <93094639+BU9D4DDY@users.noreply.github.com>
2022-05-19 23:34:53 +0200

    timer.h

---
+++ src/headers/Timer.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Module:Timer
+ *
+ * File Name: Timer.h
+ *
+ * Description: Header file for Timer
+ *
+ * Authors: Ola Mohamed - Abdelrhman Atef - Abdelrhman Hesham -Diaa Eldin Hassan -Ahmed Gamal - Rammzi Muhammed
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ *                      Function Prototypes                                    *
+ *******************************************************************************/
+	
+void Timer2_delay(unsigned long period);
+
+
+
